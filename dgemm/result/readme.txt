c0dbe9206b7a926acf89a114775b748ff6f30545
sadsock <mantis-shrimp@outlook.com>
2023-05-04 15:38:06 +0800

    dgemm 13600K

--- dgemm/result/readme.txt
+++ dgemm/result/readme.txt
@@ -1,2 +1,3 @@
 9700H openblas 0.3.23  macOS 13.3.1  Apple clang version 14.0.3
-Yitian710 blas 3.9.0   ubuntu 20.04.06 gcc 9.4.0   
+Yitian710 blas 3.9.0   ubuntu 20.04.06 gcc 9.4.0
+13600K blas 3.10.0 ubuntu 22.04.02 gcc 11.3.0  0.312824s
--- dgemm/x86.c
+++ dgemm/x86.c
@@ -1,32 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cblas.h>
+#include <time.h>
 
 int main() {
-    int n = 1000, m = 1000, k = 1000;  // 定义矩阵的维度
+  int n = 1000, m = 1000, k = 1000; // 定义矩阵的维度
 
-    // 动态分配内存并读取矩阵数据
-    double *a, *b, *c;
-    a = (double *)malloc(n * k * sizeof(double));
-    b = (double *)malloc(k * m * sizeof(double));
-    c = (double *)malloc(n * m * sizeof(double));
-    FILE *fp;
-    fp = fopen("data.bin", "rb");
-    fread(a, sizeof(double), n * k, fp);
-    fread(b, sizeof(double), k * m, fp);
-    fclose(fp);
+  clock_t start, end;
+  double cpu_time_used;
 
-    // 计算矩阵乘法
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, m, k, 1.0, a, k, b, m, 0.0, c, m);
+  // 动态分配内存并读取矩阵数据
+  double *a, *b, *c;
+  a = (double *)malloc(n * k * sizeof(double));
+  b = (double *)malloc(k * m * sizeof(double));
+  c = (double *)malloc(n * m * sizeof(double));
+  FILE *fp;
+  fp = fopen("data.bin", "rb");
 
-    // 将结果保存到文件中
-    fp = fopen("x86.bin", "wb");
-    fwrite(c, sizeof(double), n * m, fp);
-    fclose(fp);
+  size_t result;
+  result = fread(a, sizeof(double), n * k, fp);
+  if (result != n * k) {
+    printf("Failed to read file\n");
+    return 1;
+  }
+  result = fread(b, sizeof(double), k * m, fp);
+  if (result != k * m) {
+    printf("Failed to read file\n");
+    return 1;
+  }
 
-    // 释放内存
-    free(a);
-    free(b);
-    free(c);
-    return 0;
-    }
+  fclose(fp);
+
+  start = clock();
+  // 计算矩阵乘法
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, m, k, 1.0, a, k, b,
+              m, 0.0, c, m);
+  end = clock();
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("CPU Time Used: %f seconds\n", cpu_time_used);
+
+  // 将结果保存到文件中
+  fp = fopen("x86.bin", "wb");
+  fwrite(c, sizeof(double), n * m, fp);
+  fclose(fp);
+
+  // 释放内存
+  free(a);
+  free(b);
+  free(c);
+  return 0;
+}
