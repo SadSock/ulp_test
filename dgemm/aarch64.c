367ee43c724581d678c7d3f4cc8ee2f369b6495b
sadsock <mantis-shrimp@outlook.com>
2023-05-04 16:45:35 +0800

    dgemm yintian710_openblas

--- dgemm/aarch64.c
+++ dgemm/aarch64.c
@@ -1,41 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cblas.h>
+#include <time.h>
 
 int main() {
-    int n = 1000, m = 1000, k = 1000;  // 定义矩阵的维度
+  int n = 1000, m = 1000, k = 1000; // 定义矩阵的维度
 
-    // 动态分配内存并读取矩阵数据
-    double *a, *b, *c;
-    a = (double *)malloc(n * k * sizeof(double));
-    b = (double *)malloc(k * m * sizeof(double));
-    c = (double *)malloc(n * m * sizeof(double));
-    FILE *fp;
-    fp = fopen("data.bin", "rb");
-    size_t result;
-    result = fread(a, sizeof(double), n * k, fp);
-        if (result != n *k) {
-        printf("Failed to read file\n");
-        return 1;
-    }
-    result = fread(b, sizeof(double), k * m, fp);
-        if (result != k * m) {
-        printf("Failed to read file\n");
-        return 1;
-    }
-    fclose(fp);
+ clock_t start, end;
+  double cpu_time_used;
 
-    // 计算矩阵乘法
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, m, k, 1.0, a, k, b, m, 0.0, c, m);
+  // 动态分配内存并读取矩阵数据
+  double *a, *b, *c;
+  a = (double *)malloc(n * k * sizeof(double));
+  b = (double *)malloc(k * m * sizeof(double));
+  c = (double *)malloc(n * m * sizeof(double));
+  FILE *fp;
+  fp = fopen("data.bin", "rb");
+  size_t result;
+  result = fread(a, sizeof(double), n * k, fp);
+  if (result != n * k) {
+    printf("Failed to read file\n");
+    return 1;
+  }
+  result = fread(b, sizeof(double), k * m, fp);
+  if (result != k * m) {
+    printf("Failed to read file\n");
+    return 1;
+  }
+  fclose(fp);
 
-    // 将结果保存到文件中
-    fp = fopen("./result/aarch64.bin", "wb");
-    fwrite(c, sizeof(double), n * m, fp);
-    fclose(fp);
+  start = clock();
+  // 计算矩阵乘法
+  cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, m, k, 1.0, a, k, b,
+              m, 0.0, c, m);
+  end = clock();
+  cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
+  printf("CPU Time Used: %f seconds\n", cpu_time_used);
 
-    // 释放内存
-    free(a);
-    free(b);
-    free(c);
-    return 0;
-    }
+  // 将结果保存到文件中
+  fp = fopen("aarch64.bin", "wb");
+  fwrite(c, sizeof(double), n * m, fp);
+  fclose(fp);
+
+  // 释放内存
+  free(a);
+  free(b);
+  free(c);
+  return 0;
+}
--- dgemm/result/readme.txt
+++ dgemm/result/readme.txt
@@ -1,4 +1,5 @@
 9700H openblas 0.3.23  macOS 13.3.1  Apple clang version 14.0.3
-Yitian710 blas 3.9.0   ubuntu 20.04.06 gcc 9.4.0
+Yitian710 blas 3.9.0   ubuntu 20.04.06 gcc 9.4.0 0.097814s
+Yitian710 openblas 0.3.8   ubuntu 20.04.06 gcc 9.4.0 0.469796s
 13600K blas 3.10.0 ubuntu 22.04.02 gcc 11.3.0  0.242256s
 13600K_openblas  openblas 3.10.0 ubuntu 22.04.02 gcc 11.3.0  0.241172s
